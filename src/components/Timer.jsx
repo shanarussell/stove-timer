530431edd0a02ef1833e8ca227409bf0ab437f9c
Shana Russell <shana0515@gmail.com>
2022-12-18 10:48:33 -0600

    blinker functional

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,39 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import useSound from "use-sound";
+import styled, { css } from "styled-components";
 import BottomLeft from "../sounds/bottom-left.mp3";
 import BottomRight from "../sounds/bottom-right.mp3";
 import UpperLeft from "../sounds/upper-left.mp3";
 import UpperRight from "../sounds/upper-right.mp3";
 
+const Blinker = styled.div(
+  ({ isFinished }) => css`
+    ${isFinished &&
+    css`
+      background-color: #e7f922;
+      border-radius: 50%;
+      animation: blinker 1s linear infinite;
+    `}
+  `
+);
+
 export default function Timer({ burnerName, burnerClassName }) {
   // State to hold the remaining time in seconds
   const [remainingTime, setRemainingTime] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
   const [initialTime, setInitialTime] = useState("");
   const [BottomLeftSound] = useSound(BottomLeft);
   const [BottomRightSound] = useSound(BottomRight);
   const [UpperLeftSound] = useSound(UpperLeft);
   const [UpperRightSound] = useSound(UpperRight);
 
-  const onTimeExpired = () => {
-    playSound();
-    setIsRunning(false);
-    setRemainingTime(null);
-  };
-
-  function playSound() {
-    burnerName === "burner-1" && UpperLeftSound();
-    burnerName === "burner-2" && UpperRightSound();
-    burnerName === "burner-3" && BottomLeftSound();
-    burnerName === "burner-4" && BottomRightSound();
-  }
-
-  const resetTimer = () => {
+  function resetTimer() {
     setIsRunning(false);
     setRemainingTime(null);
     setInitialTime("");
-  };
+    setIsFinished(false);
+  }
 
   const startTimer = () => {
     setIsRunning(true);
@@ -54,9 +55,23 @@ export default function Timer({ burnerName, burnerClassName }) {
     }
 
     if (remainingTime === 0) {
-      onTimeExpired();
+      setIsRunning(false);
+      setRemainingTime(null);
+      setInitialTime("");
+      setIsFinished(true);
+      burnerName === "burner-1" && UpperLeftSound();
+      burnerName === "burner-2" && UpperRightSound();
+      burnerName === "burner-3" && BottomLeftSound();
+      burnerName === "burner-4" && BottomRightSound();
     }
-  }, [remainingTime]);
+  }, [
+    BottomLeftSound,
+    BottomRightSound,
+    UpperLeftSound,
+    UpperRightSound,
+    burnerName,
+    remainingTime,
+  ]);
 
   const handleElapsedTime = () => {
     const minutes = Math.floor(remainingTime / 60).toString();
@@ -72,32 +87,34 @@ export default function Timer({ burnerName, burnerClassName }) {
   // Render the remaining time in the format MM:SS
   return (
     <div className={burnerClassName}>
-      <div className={isRunning ? "circle3-glowing" : "circle3"}>
-        <div className={isRunning ? "circle2-glowing" : "circle2"}>
-          <div className={isRunning ? "circle1-glowing" : "circle1"}>
-            <input
-              className="input"
-              type="number"
-              step={1}
-              value={initialTime}
-              onChange={handleTimeInput}
-              placeholder="minutes"
-            />
-            <div className="elapsed-time">
-              {isRunning && handleElapsedTime()}
-            </div>
+      <Blinker isFinished={isFinished}>
+        <div className={isRunning ? "circle3-glowing" : "circle3"}>
+          <div className={isRunning ? "circle2-glowing" : "circle2"}>
+            <div className={isRunning ? "circle1-glowing" : "circle1"}>
+              <input
+                className="input"
+                type="number"
+                step={1}
+                value={initialTime}
+                onChange={handleTimeInput}
+                placeholder="minutes"
+              />
+              <div className="elapsed-time">
+                {isRunning && handleElapsedTime()}
+              </div>
 
-            <div className="buttons">
-              <button className="start-button" onClick={startTimer}>
-                Start
-              </button>
-              <button className="reset-button" onClick={resetTimer}>
-                Reset
-              </button>
+              <div className="buttons">
+                <button className="start-button" onClick={startTimer}>
+                  Start
+                </button>
+                <button className="reset-button" onClick={resetTimer}>
+                  Reset
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Blinker>
     </div>
   );
 }
